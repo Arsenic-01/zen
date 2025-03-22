057253656089e8781e23356ebdd86355779c88de
Arsenic-01 <vedbhor25@gmail.com>
2025-03-23 00:41:05 +0530

    updated links + description of project cards

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,103 +1,108 @@
-import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
-import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
-import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
-import Image from "next/image";
-import CheckCircleIcon from "@/assets/icons/check-circle.svg";
-import ArrowRightIcon from "@/assets/icons/arrow-up-right.svg";
-import grainImage from "@/assets/images/grain.jpg";
-import LinkIcon from "@/assets/icons/link-solid.svg";
-import SectionHeader from "@/components/SectionHeader";
-import Card from "@/components/Card";
+import darkSaasLandingPage from '@/assets/images/dark-saas-landing-page.png';
+import lightSaasLandingPage from '@/assets/images/light-saas-landing-page.png';
+import aiStartupLandingPage from '@/assets/images/ai-startup-landing-page.png';
+import Image from 'next/image';
+import CheckCircleIcon from '@/assets/icons/check-circle.svg';
+import grainImage from '@/assets/images/grain.jpg';
+import SectionHeader from '@/components/SectionHeader';
+import Card from '@/components/Card';
+import { ArrowRightIcon, CodeXml } from 'lucide-react';
 
 const portfolioProjects = [
   {
-    company: "Acme Corp",
-    year: "2022",
-    title: "Dark Saas Landing Page",
+    company: 'SBA Inc.',
+    year: '2024-25',
+    title: 'Landing Page for Architectural Firm',
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+      { title: 'Comprehensive service showcase' },
+      { title: 'Seamless appointment booking' },
+      { title: 'Enhanced user engagement by 60%' },
+      // { title: 'Improved lead conversions' },
     ],
-    link: "https://youtu.be/4k7IdSLxh6w",
+    link: 'https://www.sunilbhor.com/',
+    src: 'https://github.com/Arsenic-01/SBA',
     image: darkSaasLandingPage,
   },
   {
-    company: "Innovative Co",
-    year: "2021",
-    title: "Light Saas Landing Page",
+    company: 'K.K.Wagh Polytechnic',
+    year: '2025',
+    title: 'StudyStack',
     results: [
-      { title: "Boosted sales by 20%" },
-      { title: "Expanded customer reach by 35%" },
-      { title: "Increased brand awareness by 15%" },
+      { title: 'Helped 300+ students access notes' },
+      { title: 'Enabled seamless teacher uploads' },
+      { title: 'Improved study efficiency for students' },
     ],
-    link: "https://youtu.be/7hi5zwO75yc",
+    link: 'https://studystack01.vercel.app/',
+    src: 'https://github.com/Arsenic-01/studystack',
     image: lightSaasLandingPage,
   },
   {
-    company: "Quantum Dynamics",
-    year: "2023",
-    title: "AI Startup Landing Page",
+    company: 'Hobby Project',
+    year: '2025',
+    title: 'Cloudgram',
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+      { title: 'Unlimited 20MB file uploads' },
+      { title: 'Fast 2GB uploads via S3' },
+      { title: 'Secure Telegram integration' },
+      { title: 'Scalable and user-friendly' },
     ],
-    link: "https://youtu.be/Z7I5uSRHMHg",
+    link: 'https://cloudgram-01.vercel.app/',
+    src: 'https://github.com/Arsenic-01/cloudgram',
     image: aiStartupLandingPage,
   },
 ];
 
 export const ProjectsSection = () => {
   return (
-    <section className="pb-16 lg:py-24" id="projects">
-      <div className="container">
+    <section className='pb-16 lg:py-24' id='projects'>
+      <div className='container'>
         <SectionHeader
-          eyebrow="Real-World Results"
-          title="Featured Projects"
-          description="See how I transformed concepts into engaging digital experiences."
+          eyebrow='Real-World Results'
+          title='Featured Projects'
+          description='See how I transformed concepts into engaging digital experiences.'
         />
-        <div className="flex flex-col mt-10 md:mt-20 gap-20">
+        <div className='flex flex-col mt-10 md:mt-20 gap-20'>
           {portfolioProjects.map((project, projectIndex) => (
             <Card
-              className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:px-20 lg:pt-16 sticky"
+              className='px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:px-20 lg:pt-16 sticky'
               key={project.title}
               style={{
                 top: `calc(64px + ${projectIndex * 50}px)`,
               }}
             >
-              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
-                <div className="lg:pb-16">
-                  <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text inline-flex font-bold uppercase tracking-widest text-sm gap-2">
+              <div className='lg:grid lg:grid-cols-2 lg:gap-16'>
+                <div className='lg:pb-16'>
+                  <div className='bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text inline-flex font-bold uppercase tracking-widest text-sm gap-2'>
                     <span>{project.company}</span>
                     <span>&bull;</span>
                     <span>{project.year}</span>
                   </div>
-                  <h3 className="font-serif text-2xl mt-2 md:text-4xl md:mt-4">
+                  <h3 className='font-serif text-2xl mt-2 md:text-4xl md:mt-4'>
                     {project.title}
                   </h3>
-                  <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
-                  <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                  <hr className='border-t-2 border-white/5 mt-4 md:mt-5' />
+                  <ul className='flex flex-col gap-4 mt-4 md:mt-5'>
                     {project.results.map((result) => (
                       <li
                         key={result.title}
-                        className="flex text-sm md:text-base text-white/50 gap-2"
+                        className='flex text-sm md:text-base text-white/50 gap-2'
                       >
-                        <CheckCircleIcon className="size-5 md:size-6" />
+                        <CheckCircleIcon className='size-5 md:size-6' />
                         <span>{result.title}</span>
                       </li>
                     ))}
                   </ul>
-                  <div className="flex gap-3 w-full">
-                    <a href={project.link} target="_blank">
-                      <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-8 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
-                        <span>View Live Site</span>
-                        <ArrowRightIcon className="size-4" />
+                  <div className='flex flex-col md:flex-row gap-3 w-full'>
+                    <a href={project.link} target='_blank'>
+                      <button className='bg-white text-gray-950 h-12 w-full md:w-auto px-5 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8'>
+                        <span>Live Site</span>
+                        <ArrowRightIcon className='size-4 -rotate-45' />
                       </button>
                     </a>
-                    <a href={project.link} target="_blank">
-                      <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
-                        <LinkIcon className="size-5 lg:size-6" />
+                    <a href={project.src} target='_blank'>
+                      <button className='bg-white text-gray-950 rounded-xl h-12 w-full md:w-auto px-5 font-semibold inline-flex items-center justify-center gap-2 md:mt-8'>
+                        <span>Code</span>
+                        <CodeXml className='size-4' />
                       </button>
                     </a>
                   </div>
@@ -106,7 +111,7 @@ export const ProjectsSection = () => {
                   <Image
                     src={project.image}
                     alt={project.title}
-                    className="mt-8 -mb-4 md:mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
+                    className='mt-8 -mb-4 md:mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none'
                   />
                 </div>
               </div>
